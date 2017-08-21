0850f55b89761c462b15ea0dd24ba8757529fd06
Benjamin Thomas <benjaminthomas1999@outlook.com>
2017-08-21 08:52:44 +0100

    Reducing Redundancies

--- sketch_apr06a/sketch_apr06a.ino/sketch_apr06a.ino.ino
+++ sketch_apr06a/sketch_apr06a.ino/sketch_apr06a.ino.ino
@@ -3,21 +3,15 @@
 #include "Adafruit_LEDBackpack.h"
 #include <TimeLib.h>
 
-
-int programCounter = 0;
-
-void incrementProgramCounter(){
-  programCounter++;
-  if(programCounter > 4) programCounter = 0;
-}
-
-
 class Button{
   public:int pin;
   public:bool history[2] = {0, 0};
+  public:bool pressed = false;
+  
   void updateHistory(){
     this->history[0] = this->history[1];
     this->history[1] = !digitalRead(this->pin);
+    this->pressed = this->history[1];
   }  
   bool state(){
     if((this->history[1]) && (!this->history[0])) return true;
@@ -27,6 +21,21 @@ class Button{
     this->pin = pinToSetTo;
     pinMode(this->pin, INPUT);
     digitalWrite(this->pin, HIGH);
+  }
+  bool clickOrHold(){ //0 for click, 1 for hold
+    int startTime = second();
+    int duration = 0;
+    while(this->pressed){
+      duration = second()-startTime;
+      if(duration > 1) break;
+      this->updateHistory();
+    }
+    if(duration > 1){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
   
 };
@@ -80,7 +89,7 @@ void progressTime(){
 
 void binaryTime(){
   matrix.clear();
-  matrix.setRotation(1);//top left is (0,0)
+  matrix.setRotation(1);//so top left is (0,0)
   uint8_t screen[] =
   { month(),
     0,
@@ -111,183 +120,50 @@ void displayedTime(){
   matrix.setTextSize(1);
   matrix.setTextWrap(false);
   matrix.setTextColor(LED_ON);
-  
-  for (int8_t x = 2; x >= -23; x--) {
+  for (int8_t x = 7; x >= -28; x--) {
     buttonInput();
     if(btn3.state()){
-      incrementProgramCounter();
       return;
     }
-    
     matrix.clear();
-    matrix.setCursor(x,0);
     
     String prettyTime = getPrettyTime();
-    matrix.print(prettyTime);
-    matrix.writeDisplay();
-    
-    
-    
-    delay(70);
-  }
-  for (int8_t x= -23; x <= 2; x++) {
-    buttonInput();
-    if(btn3.state()){
-      incrementProgramCounter();
-      return;
-    }
-    matrix.clear();
     matrix.setCursor(x,0);
-    
-    String prettyTime = getPrettyTime();
     matrix.print(prettyTime);
     matrix.writeDisplay();
-    
-
-    delay(70);
+    delay(60);
   }
 }
-void pong(){
-  matrix.setRotation(1);
-  class Paddle;
-  class Ball{
-    public:float velocity[2] = {1, -1};
-    public:float position[2] = {4, 3};
-    
-    void movePosition(float enemyPosition[2], float playerPosition[2]){       
-      if(this->position[0] >= 7){//screen protection
-         this->position[0] = 7;
-         this->velocity[0] = -this->velocity[0];
-      }else if(this->position[0] <= 0){
-         this->position[0] = 0;
-         this->velocity[0] = -this->velocity[0];
-      }
-       
-      if(this->position[1] > 8){//screen protection
-         this->position[0] = random(1,6);
-         this->velocity[1] = -this->velocity[1];
-         this->position[1] = 3;
-      }else if(this->position[1] < -1){
-         this->position[0] = random(1,6);
-         this->position[1] = 3;
-      }
 
-       
-      if(this->position[1] == 1 && this->velocity[1] < 0){
-        if(this->position[0] == enemyPosition[0] || this->position[0] == enemyPosition[0]+1){
-           this->velocity[1] = -this->velocity[1];
-        }
-        else if(this->position[0] == enemyPosition[0]-1){
-          if(this->velocity[0] > 0){
-            this->velocity[1] = -this->velocity[1];
-          }
-        }
-        else if(this->position[0] == enemyPosition[0]+2){
-          if(this->velocity[0] < 0){
-            this->velocity[1] = -this->velocity[1];
-          }
-        }
-      }
-
-      if(this->position[1] == 6 && this->velocity[1] > 0){
-        if(this->position[0] == playerPosition[0] || this->position[0] == playerPosition[0]+1){
-           this->velocity[1] = -this->velocity[1];
-        }
-        else if(this->position[0] == playerPosition[0]-1){
-          if(this->velocity[0] > 0){
-            this->velocity[1] = -this->velocity[1];
-          }
-        }
-        else if(this->position[0] == playerPosition[0]+2){
-          if(this->velocity[0] < 0){
-            this->velocity[1] = -this->velocity[1];
-          }
-        }
-      }
-
-      
-      this->position[0] += this->velocity[0];
-      this->position[1] += this->velocity[1];
-      
-    }
-    void draw(){
-      matrix.drawPixel(this->position[0], this->position[1], 1);
-    }
-  };
-
-
- 
-  class Paddle{
-    public:float position[2] = {3, 0};//leftmost of paddle ***
-    
-    void ai(Ball &ball){
-      bool outBounds = false;
-       if(ball.velocity[1] < 0){
-        if(ball.position[0] < this->position[0]){
-          this->position[0]--;
-          outBounds = true;
-        }
-        else if(ball.position[0] > this->position[0]+1){
-          this->position[0]++;
-          outBounds = true;
-        }
-      }
-      
-   }
-   void draw(){
-      matrix.drawPixel(this->position[0], this->position[1], 1);
-      matrix.drawPixel(this->position[0]+1, this->position[1], 1);
-   }
-  };
-  
-  
-  Ball ball;
-  Paddle enemy;
-  Paddle player;
-  player.position[1] = 7;
-  
-  while(true){
-    buttonInput();
-    matrix.clear();
-    ball.movePosition(enemy.position, player.position);
-    ball.draw();
-    
-    enemy.ai(ball);
-    enemy.draw();
-
-    
-    if(btn2.history[1]) player.position[0]--;
-    if(btn1.history[1]) player.position[0]++;
-    if(btn3.state()){
-      incrementProgramCounter();
-      return;
-    }
-    player.draw();
-
-    matrix.writeDisplay();
-    for(int i = 0; i < 200; i++){
-      delay(1);
-      buttonInput();
-      if(btn3.state()){
-        incrementProgramCounter();
-        return;
-      }
-    }
-  }
-}
 
 void loop(){
+  bool gameMode = false;
   buttonInput();
-  if(btn3.state()){
-    incrementProgramCounter();
+  
+  if(btn3.state()){//determine click or hold
+    if(btn3.clickOrHold()){
+      gameMode = true;
+    }
+    else{
+      displayedTime();
+    }
   }
-  if(programCounter == 0) binaryTime();
-  if(programCounter == 1) progressTime();
-  if(programCounter == 2) displayedTime();
-  if(programCounter == 3) pong();
-  if(programCounter == 4){
-    matrix.clear();
-    matrix.writeDisplay();  
-  };
 
+  while(gameMode){
+    buttonInput();
+    if(btn3.state()){
+      if(btn3.clickOrHold()) gameMode = false;  
+    }
+    
+    matrix.clear();
+    matrix.drawPixel(0, 0, second()%2);
+    matrix.writeDisplay();
+
+    
+  }
+
+  
+  
+  matrix.clear();
+  matrix.writeDisplay();
 }
