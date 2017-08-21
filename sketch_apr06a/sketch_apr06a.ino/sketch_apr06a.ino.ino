fc4a97566dd0285f60175c5e0a08053ad3c072b0
Benjamin Thomas <benjaminthomas1999@outlook.com>
2017-08-21 08:58:34 +0100

    Reducing Redundancies

--- sketch_apr06a/sketch_apr06a.ino/sketch_apr06a.ino.ino
+++ sketch_apr06a/sketch_apr06a.ino/sketch_apr06a.ino.ino
@@ -2,6 +2,8 @@
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
 #include <TimeLib.h>
+Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
+
 
 class Button{
   public:int pin;
@@ -39,52 +41,14 @@ class Button{
   }
   
 };
-
 Button btn1;
 Button btn2;
 Button btn3;
-
 
 void buttonInput(){
   btn1.updateHistory();
   btn2.updateHistory();
   btn3.updateHistory();
-}
-
-Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
-void setup() {
-  matrix.begin(0x70);
-  matrix.setBrightness(16);
-  setTime(1491678866);
-  
-  btn1.init(12);
-  btn2.init(9);
-  btn3.init(11);
-}
-
-void progressTime(){
-  matrix.setRotation(3);//bottom left is (0,0)
-  matrix.clear();
-  for(int i = 0; i < second()%8; i++){
-    matrix.drawPixel(i, 0, 1);  
-  }
-  for(int i = 0; i < floor(second()/8); i++){
-    matrix.drawPixel(i, 1, 1);
-  }
-  for(int i = 0; i < minute()%8; i++){
-    matrix.drawPixel(i, 3, 1);  
-  }
-  for(int i = 0; i < floor(minute()/8); i++){
-    matrix.drawPixel(i, 4, 1);
-  }
-  
-  for(int i = 0; i < hour()%8; i++){
-    matrix.drawPixel(i, 6, 1);
-  }
-  for(int i = 0; i < floor(hour()/8); i++){
-    matrix.drawPixel(i, 7, 1);
-  }
-  matrix.writeDisplay();
 }
 
 void binaryTime(){
@@ -103,9 +67,7 @@ void binaryTime(){
 
 String getPrettyTime(){
   String prettyTime;
-  if(hour()<10){
-    prettyTime += "0";
-  }
+  
   prettyTime += String(hour())+":";
   if(minute()<10){
     prettyTime += "0";  
@@ -126,14 +88,23 @@ void displayedTime(){
       return;
     }
     matrix.clear();
-    
-    String prettyTime = getPrettyTime();
     matrix.setCursor(x,0);
-    matrix.print(prettyTime);
+    matrix.print(getPrettyTime(););
     matrix.writeDisplay();
     delay(60);
   }
 }
+
+void setup() {
+  matrix.begin(0x70);
+  matrix.setBrightness(16);
+  setTime(1491678866);
+  
+  btn1.init(12);
+  btn2.init(9);
+  btn3.init(11);
+}
+
 
 
 void loop(){
@@ -161,9 +132,6 @@ void loop(){
 
     
   }
-
-  
-  
   matrix.clear();
   matrix.writeDisplay();
 }
